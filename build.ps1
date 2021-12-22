5b3cf5cef1eae96b7701563d7e90f0d295e0d2c7
stahir80td <96396056+stahir80td@users.noreply.github.com>
2021-12-21 21:47:03 -0500

    Add files via upload

--- build.ps1
+++ build.ps1
@@ -1,10 +1,21 @@
 ﻿$dirs = get-childitem 'C:\Program Files (x86)' -Directory | where { $_.Name -like '*Microsoft Visual*'}
 
+$foundit = $null
+
 foreach($dir in $dirs)
 {
-    Get-ChildItem "$($dir.FullName)" -Recurse | where {$_.Name -eq "devenv.com"}
+    $foundit = Get-ChildItem "$($dir.FullName)" -Recurse | where {$_.Name -eq "devenv.com"}
 }
 
-#& "$($foundit.FullName)" "Mining For Gold - Workflow.sln" /build Debug
+Write-host "$foundit"
 
-#& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" Source\MySolution.sln /build Release /project Source\My\Setup\Project.vdproj"
+try{
+    & "$($foundit.FullName)" "Mining For Gold - Workflow.sln" /build Debug
+}
+catch{
+
+    Write-Host "Dynamic didn't work, now trying hard-coded path"
+    
+    & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.com" "Mining For Gold - Workflow.sln" /build Debug
+
+}
