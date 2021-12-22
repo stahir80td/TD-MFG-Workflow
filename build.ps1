d2a5fb805fd3ecb5d1836f1052c8dd311d51f738
stahir80td <96396056+stahir80td@users.noreply.github.com>
2021-12-21 21:40:10 -0500

    Add files via upload

--- build.ps1
+++ build.ps1
@@ -2,9 +2,9 @@
 
 foreach($dir in $dirs)
 {
-    $foundit = Get-ChildItem "$($dir.FullName)" -Recurse | where {$_.Name -eq "devenv.com"}  | select -First 1
+    Get-ChildItem "$($dir.FullName)" -Recurse | where {$_.Name -eq "devenv.com"}
 }
 
-& "$($foundit.FullName)" "Mining For Gold - Workflow.sln" /build Debug
+#& "$($foundit.FullName)" "Mining For Gold - Workflow.sln" /build Debug
 
 #& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" Source\MySolution.sln /build Release /project Source\My\Setup\Project.vdproj"
