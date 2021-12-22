6f3b06cc9cfa128fa5614c4228b70805c8d50934
stahir80td <96396056+stahir80td@users.noreply.github.com>
2021-12-21 21:36:17 -0500

    Add files via upload

--- build.ps1
+++ build.ps1
@@ -1,1 +1,10 @@
-Write-Host "Hello World"
+﻿$dirs = get-childitem 'C:\Program Files (x86)' -Directory | where { $_.Name -like '*Microsoft Visual*'}
+
+foreach($dir in $dirs)
+{
+    $foundit = Get-ChildItem "$($dir.FullName)" -Recurse | where {$_.Name -eq "devenv.com"}  | select -First 1
+}
+
+& "$($foundit.FullName)" "Mining For Gold - Workflow.sln" /build Debug
+
+#& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" Source\MySolution.sln /build Release /project Source\My\Setup\Project.vdproj"
